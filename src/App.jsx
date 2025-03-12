4d65ccc156379d941547e5449cf1c3e53b2bb739
soeun823 <bsoeun866@gmail.com>
2025-03-13 08:08:48 +0900

    build :: 환경설정 및 적용 테스트

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Key from './components/Key'
+
+function App() {
+
+  return (
+    <>
+      <Key/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Key/index.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Key() {
+  return (
+    <Spline scene="https://prod.spline.design/lGpAy1Of5E6nAlji/scene.splinecode" />
+  );
+}
